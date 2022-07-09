bace8acd3b75e699b8c9db9dffb98362ed0fc992
= <=>
2022-07-09 20:32:48 +1000

    create the initial project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+function App() {
+  return <Hero />;
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "hero.css";
+const Hero = () => {
+  <div className="hero">
+    <div className="left-h"></div>
+    <div className="right-h"></div>
+  </div>;
+};
+
+export default Hero;
