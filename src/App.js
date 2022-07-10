5c9e6d89870e8288fd04ee2dea56a386db6ef17a
= <=>
2022-07-10 23:15:05 +1000

    adding components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Program/Programs";
+import Reason from "./components/Reason/Reason";
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
+      <Reason />
     </div>
   );
 }
